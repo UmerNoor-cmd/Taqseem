bcd38595b31f5d0d40e6cb532bf5488f73783da8
hanizehra <shzk1214@gmail.com>
2025-05-01 01:55:44 +0500

    "Added NGO Alert Screen"

--- lib/screens.dart
+++ lib/screens.dart
@@ -2,3 +2,4 @@ export 'screens/give_help/give_help_screen.dart';
 export 'screens/home/home_screen.dart';
 export 'screens/Success_Donation/Donation.dart';
 export 'screens/Alert/Alert_page.dart';
+export 'screens/Alert/Alert_page_ngo.dart';
